c622ac62101af7b6042a312839c2689da47804d0
Naveen Malik <nmalik@redhat.com>
2014-06-10 10:27:06 -0400

    Updated all.sh to run a new all.yaml that imports all the metadata and crud tests

--- functional/all.sh
+++ functional/all.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-pushd metadata > /dev/null
-. ./metadata.sh $@ || exit $?
-popd > /dev/null
-pushd crud > /dev/null
-./crud.sh $@ || exit $?
-popd > /dev/null
+# requires resttest (https://github.com/lightblue-platform/pyresttest) and argparse module
 
+if [ "x$1" == "x" ]; then
+    echo "Must specify base URL as first command line argument."
+    exit 1
+fi
+
+# Global:
+LOGGING_LEVEL="debug"
+if [ "x$2" != "x" ]; then
+    LOGGING_LEVEL=$2
+fi
+export ENTITY_NAME="nmalik-$(date +'%Y%m%d%H%M%S')";
+
+# Metadata:
+export ENTITY_VERSION_1="1.0.0";
+export ENTITY_VERSION_2="2.0.0"
+
+# CRUD:
+export ENTITY_VERSION="${ENTITY_VERSION_2}"
+
+echo "Running tests for new entity: $ENTITY_NAME"
+
+python -c "import resttest; resttest.main('$1', 'all.yaml', '$LOGGING_LEVEL')" 2>&1
+
+#unset ENTITY_NAME
+#unset ENTITY_VERSION_1
+#unset ENTITY_VERSION_2
