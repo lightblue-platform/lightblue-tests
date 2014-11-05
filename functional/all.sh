977e5e3d60d36d4d351ac9335115f1bc15adb411
Luan Cestari <luan.cestari@gmail.com>
2014-11-05 10:25:47 -0200

    Changes to fucntional test work with 2 URLS (one for data and other for metadata) Part 2

--- functional/all.sh
+++ functional/all.sh
@@ -3,15 +3,20 @@
 # requires resttest (https://github.com/lightblue-platform/pyresttest) and argparse module
 
 if [ "x$1" == "x" ]; then
-    echo "Must specify base URL as first command line argument."
+    echo "Must specify base URL for data as first command line argument."
+    exit 1
+fi
+
+if [ "x$2" == "x" ]; then
+    echo "Must specify base URL for metadata as second command line argument."
     exit 1
 fi
 
 # Global:
 INTERACTIVE="False"
 LOGGING_LEVEL="debug"
-if [ "x$2" != "x" ]; then
-    LOGGING_LEVEL=$2
+if [ "x$3" != "x" ]; then
+    LOGGING_LEVEL=$3
 fi
 export ENTITY_NAME="nmalik-$(date +'%Y%m%d%H%M%S')";
 
@@ -24,9 +29,9 @@ export ENTITY_VERSION="${ENTITY_VERSION_2}"
 
 echo "Running tests for new entity: $ENTITY_NAME"
 
-python -c "import resttest; args=dict(); args['url']='$1'; args['test']='step1_metadata.yaml'; args['log']='$LOGGING_LEVEL'; args['interactive']=$INTERACTIVE; args['print_bodies']=$INTERACTIVE; resttest.main(args)" 2>&1
+python -c "import resttest; args=dict(); args['url']='$2'; args['test']='step1_metadata.yaml'; args['log']='$LOGGING_LEVEL'; args['interactive']=$INTERACTIVE; args['print_bodies']=$INTERACTIVE; resttest.main(args)" 2>&1
 python -c "import resttest; args=dict(); args['url']='$1'; args['test']='step2_data.yaml'; args['log']='$LOGGING_LEVEL'; args['interactive']=$INTERACTIVE; args['print_bodies']=$INTERACTIVE; resttest.main(args)" 2>&1
-python -c "import resttest; args=dict(); args['url']='$1'; args['test']='step3_metadata_teardown.yaml'; args['log']='$LOGGING_LEVEL'; args['interactive']=$INTERACTIVE; args['print_bodies']=$INTERACTIVE; resttest.main(args)" 2>&1
+python -c "import resttest; args=dict(); args['url']='$2'; args['test']='step3_metadata_teardown.yaml'; args['log']='$LOGGING_LEVEL'; args['interactive']=$INTERACTIVE; args['print_bodies']=$INTERACTIVE; resttest.main(args)" 2>&1
 
 #unset ENTITY_NAME
 #unset ENTITY_VERSION_1
