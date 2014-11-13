7d2bf945b770449ef9f92f33610afbf1c7c8c643
Luan Cestari <luan.cestari@gmail.com>
2014-11-13 11:09:52 -0200

    Fixes

--- load/add_benchmark.sh
+++ load/add_benchmark.sh
@@ -9,5 +9,6 @@ metadata/metadata-setup.yaml
 metadata/metadata-teardown.yaml"
 for f in $YAMLs
 do
-	cat | sed 's/name\s*:\s*"\(.*\)"/&\n\1/g' | sed '/name\s*:\s*"/{n;s/\s/_/g}' | sed '/name\s*:\s*"/{n;s/\(.*\)/    - metrics: {namelookup_time, connect_time, appconnect_time, pretransfer_time, starttransfer_time, redirect_time, total_time, size_download, size_upload, request_size, speed_download, speed_upload, redirect_count, num_connects }\n    - warmup_runs: 0\n    - benchmark_runs: 1\n    - output_file: '"'"'\1-{thread}.csv'"'"'/g}'
+	echo $f
+	cat $f | sed 's/name\s*:\s*"\(.*\)"/&\n\1/g' | sed '/name\s*:\s*"/{n;s/\s/_/g}' | sed '/name\s*:\s*"/{n;s/\(.*\)/    - metrics: {namelookup_time, connect_time, appconnect_time, pretransfer_time, starttransfer_time, redirect_time, total_time, size_download, size_upload, request_size, speed_download, speed_upload, redirect_count, num_connects }\n    - warmup_runs: 0\n    - benchmark_runs: 1\n    - output_file: '"'"'\1-{thread}.csv'"'"'/g}' > tmpfile && mv tmpfile $f
 done
